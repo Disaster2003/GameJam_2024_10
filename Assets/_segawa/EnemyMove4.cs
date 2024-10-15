58179ee11836de7aa4a043b06b84e23b0ed0ce10
segawak <segawakeigo@gmail.com>
2024-10-16 00:31:07 +0900

    EnemyMove4

---
+++ Assets/_segawa/EnemyMove4.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemymove4 : MonoBehaviour
+{
+    public Vector2 titen;
+    public float MoveLeftSpeed;
+    public float MoveUpSpeed;
+
+    private int yoko;
+    // Start is called before the first frame update
+    void Start()
+    {
+        yoko = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < titen.x && yoko == 0)
+        {
+            transform.Translate(new Vector2(MoveLeftSpeed * Time.deltaTime, 0));
+            if (transform.position.x >= titen.x) yoko = 1;
+        }
+        else if (transform.position.x > titen.x && yoko == 0)
+        {
+            transform.Translate(new Vector2(-MoveLeftSpeed * Time.deltaTime, 0));
+            if (transform.position.x <= titen.x) yoko = 1;
+        }
+
+        if (transform.position.y < titen.y && yoko == 1)
+        {
+            transform.Translate(new Vector2(0,MoveUpSpeed * Time.deltaTime));
+            if (transform.position.y >= titen.y) Destroy(this);
+        }
+        else if (transform.position.y > titen.y && yoko == 1)
+        {
+            transform.Translate(new Vector2(0,-MoveUpSpeed * Time.deltaTime));
+            if (transform.position.y <= titen.y) Destroy(this);
+        }
+
+
+    }
+}
