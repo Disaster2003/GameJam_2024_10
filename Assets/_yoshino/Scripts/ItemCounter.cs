016777f5e798f562c76d7d85d12e3d4f99242e26
Disaster <Disaster20031017@gmail.com>
2024-10-16 10:32:01 +0900

    Update BombChargeItem

---
+++ Assets/_yoshino/Scripts/ItemCounter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCounter : MonoBehaviour
+{
+    private static ItemCounter instance; // クラスのインスタンス
+
+    private int numberItem; // アイテム数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        numberItem = 0; // アイテム数の初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// インスタンスを取得する
+    /// </summary>
+    public static ItemCounter GetInstance() { return instance; }
+
+    /// <summary>
+    /// アイテム数を取得する
+    /// </summary>
+    public int GetNumberItem() { return numberItem; }
+
+    /// <summary>
+    /// アイテム数を増やす
+    /// </summary>
+    public void IncreaseNumberItem() { if (numberItem < 20) numberItem++; }
+}
