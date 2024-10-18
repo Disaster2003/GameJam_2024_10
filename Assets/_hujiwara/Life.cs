34529e861cd2f1d758893b1029b7b3b15d0ce276
fujiwararyusei <fujiwara.ryusei1013@gmail.com>
2024-10-18 22:37:36 +0900

    coment up

--- Assets/_hujiwara/Life.cs
+++ Assets/_hujiwara/Life.cs
@@ -4,32 +4,35 @@ using UnityEngine;
 
 public class Life : MonoBehaviour
 {
-    int EnemyHP = 5;
-    GameObject Item;
+    int EnemyHP = 5;                                //敵のHP
+    GameObject Item;                                //ドロップアイテム
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    void TakeDamege(int damege)
+
+    void TakeDamege(int damege)     //敵がダメージを受けるときの処理
     {
-        EnemyHP -= damege;
-        if (EnemyHP < 0)
+        EnemyHP -= 1;          //damegeを受けたらHP減少
+        if (EnemyHP < 0)            //EnemyHPが0以下になったらDieを送る
         {
             EnemyHP = 0;
             Die();
         }
     }
-    void Die()
+
+
+    void Die()                      //敵が死亡した時の処理
     {
-        DropItem();
+        DropItem();                 //Dieが送られてきたらアイテムドロップとEnemyの削除
         Destroy(gameObject);
     }
 
     void DropItem()
     {
-        Vector3 dropposition = transform.position + new Vector3(0,1,0);
+        Vector3 dropposition = transform.position + new Vector3(0,1);         //アイテムの位置を決定Enemyの位置？
         Instantiate(Item, dropposition, Quaternion.identity);
     }
 
