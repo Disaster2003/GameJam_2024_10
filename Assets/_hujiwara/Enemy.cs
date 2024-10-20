f087bcf772bfb81ee7a850713e42e73445521cf5
fujiwararyusei <fujiwara.ryusei1013@gmail.com>
2024-10-21 01:18:20 +0900

    pubulictuika

--- Assets/_hujiwara/Enemy.cs
+++ Assets/_hujiwara/Enemy.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Life life;
+    int EnemyHP = 5;            //敵のHP
+    Rigidbody2D rb;
+    float at = 1f;              //エネミーの攻撃力
+    public float PHP = 1f;      //プレイヤーのHP(仮)
+    public float EAI = 1f;             //Enemies Appear Intervalの略，敵が出現する間隔
+    public float EAPY = 0f;             //－－positionの略,エネミーがが出現するｙ座標
+    public float EMS = 3f;             //Enemy Move Speedの略、エネミーの移動速度
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        life = GetComponent<Life>();
+        InvokeRepeating(nameof(Appear), 0f, EAI);
+    }
+   
+    void Appear()           //エネミーを右から出現させる処理
+    {
+        Vector3 EAP = new Vector3(Screen.width, EAP, 0);
+        EAP = Camera.main.ScreenToWorldPoint(EAP);
+        EAP.z = 0;
+    }
+    void Eattack(Collision collision)                  //敵の攻撃
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+           // PHP php = collision.gameObject.GetComponent<PHP>      //プレイヤーのスクリプト名がわからないので
+       Debug.Log("プレイヤーがダメージを受けた");                   //コメントにしています（エネミーがプレイヤーに
+        }                                                           //に当たった時の処理)    
+        
         
     }
 
+    void TakeDamege(int damege)     //敵がダメージを受けるときの処理
+    {
+        EnemyHP -= 1;          //damegeを受けたらHP減少
+        if (EnemyHP < 0)            //EnemyHPが0以下になったらDieを送る
+        {
+            EnemyHP = 0;
+            life.Die();
+        }
+    }
+   
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(-1f, rb.velocity.y);  //左に進む
     }
 }
--- Assets/_hujiwara/Life.cs
+++ Assets/_hujiwara/Life.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Life : MonoBehaviour
 {
-    int EnemyHP = 5;                                //敵のHP
+    float DropChance = 0.5f;        //アイテムドロップの確率 
     GameObject Item;                                //ドロップアイテム
     // Start is called before the first frame update
     void Start()
@@ -13,27 +13,28 @@ public class Life : MonoBehaviour
     }
 
 
-    void TakeDamege(int damege)     //敵がダメージを受けるときの処理
-    {
-        EnemyHP -= 1;          //damegeを受けたらHP減少
-        if (EnemyHP < 0)            //EnemyHPが0以下になったらDieを送る
-        {
-            EnemyHP = 0;
-            Die();
-        }
-    }
+   
 
 
-    void Die()                      //敵が死亡した時の処理
+    public void Die()                      //敵が死亡した時の処理
     {
+
         DropItem();                 //Dieが送られてきたらアイテムドロップとEnemyの削除
         Destroy(gameObject);
     }
 
     void DropItem()
     {
-        Vector3 dropposition = transform.position + new Vector3(0,1);         //アイテムの位置を決定Enemyの位置？
-        Instantiate(Item, dropposition, Quaternion.identity);
+        if (Random.value  < DropChance)
+        {
+            Vector3 dropposition = transform.position;               //アイテムの位置を決定Enemyの位置
+            Instantiate(Item, dropposition, Quaternion.identity);
+            Debug.Log("drop");
+        }
+        else
+        {
+            Debug.Log("not drop");
+        }
     }
 
     // Update is called once per frame
