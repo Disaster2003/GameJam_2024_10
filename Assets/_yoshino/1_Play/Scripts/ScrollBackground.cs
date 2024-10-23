8d9bd2089d8e2e10528a3af69e5bb3ca81f0e0b9
tomoryu0429 <tomonaga.ryuji@gmail.com>
2024-10-24 02:36:06 +0900

    ScrollScript_Adjust

--- Assets/_yoshino/1_Play/Scripts/ScrollBackground.cs
+++ Assets/_yoshino/1_Play/Scripts/ScrollBackground.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ScrollBackground : MonoBehaviour
 {
-    [SerializeField, Header("初期位置")]
+    [SerializeField, Header("ゲーム開始時の位置")]
+    private Vector3 startposition;
+
+    [SerializeField, Header("初期化した際の位置")]
     private Vector3 positionInitialize;
 
     [SerializeField, Header("スクロール終了位置のx座標")]
@@ -17,7 +20,7 @@ public class ScrollBackground : MonoBehaviour
     void Start()
     {
         // 初期配置
-        transform.position = positionInitialize;
+        transform.position = startposition;
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class ScrollBackground : MonoBehaviour
         // 前景のスクロール
         transform.Translate(speedMove * Time.deltaTime, 0, 0);
 
-        if (transform.position.x >= position_xEnd)
+        if (transform.position.x <= position_xEnd)
         {
             // 初期位置に戻す
             transform.position = positionInitialize;
